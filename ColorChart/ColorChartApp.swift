d174baccc3d06b43372a43367872ceac0188453d
リョウ <121738322+RyoDeveloper@users.noreply.github.com>
2023-07-23 14:50:02 +0900

    Initial Commit

---
+++ ColorChart/ColorChartApp.swift
@@ -0,0 +1,18 @@
+//
+//  ColorChartApp.swift
+//  ColorChart
+//  
+//  https://github.com/RyoDeveloper/ColorChart
+//  Copyright © 2023 RyoDeveloper. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct ColorChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
